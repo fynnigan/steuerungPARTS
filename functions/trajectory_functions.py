f6cad8344549d7e56bf467e8b4a638d5d44b39f6
fynn <fynn.heidenreich@gmail.com>
2025-05-09 00:05:11 +0200

    added synchron PTP and added Gripper to Arm

--- functions/trajectory_functions.py
+++ functions/trajectory_functions.py
@@ -86,3 +86,88 @@ class PTPTrajectory:
             lengths[i] = self.startLengths[i] + self.directions[i] * s
 
         return lengths
+    
+class syncPTPTrajectory:
+    def __init__(self, startLengths, targetLengths, vMax, aMax):
+        self.startLengths = np.array(startLengths)
+        self.targetLengths = np.array(targetLengths)
+        self.vMax = vMax
+        self.aMax = aMax
+        self.numActuators = len(startLengths)
+        self.computeTiming()
+
+    def computeTiming(self):
+        # calc length difference, direction and distance
+        self.deltas = self.targetLengths - self.startLengths
+        self.distances = np.abs(self.deltas)
+        self.directions = np.sign(self.deltas)
+        
+        # find max distance
+        self.sMax = np.max(self.distances)
+        
+        # initialize time parameters
+        # acceleration time for max velocity
+        self.t_accel = self.vMax / self.aMax
+        # distance during accerlation
+        self.s_accel = 0.5 * self.aMax * self.t_accel**2
+        
+        # true = Dreieck, false = Trapez
+        self.triangleProfile = np.zeros(self.numActuators, dtype=bool)
+        
+        # amax is not reached -> Dreieckprofil
+        if self.sMax < 2 * self.s_accel:
+            self.triangleProfile = True
+            self.t_accel = np.sqrt(self.sMax / self.aMax)
+            self.t_total_sync = 2 * self.t_accel
+        else: # amax is reached -> Trapezprofil
+            self.triangleProfile = False
+            self.t_accel = self.t_accel
+            s_const = (self.sMax - 2 * self.s_accel)
+            t_const = s_const / self.vMax
+            self.t_total_sync = 2 * self.t_accel + t_const
+            self.t_total = self.t_total_sync
+            
+        self.aMaxVec = np.zeros(self.numActuators)
+        self.vMaxVec = np.zeros(self.numActuators)
+        
+        for i in range(self.numActuators):
+            self.aMaxVec[i] = 4 * self.distances[i] / (self.t_total_sync**2)
+            self.vMaxVec[i] = self.aMaxVec[i] * (self.t_total_sync / 2)
+        
+
+    # returns the length for all actuators for a specific time stamp
+    def evaluate(self, t):
+        lengths = np.zeros(self.numActuators)
+    
+        for i in range(self.numActuators):
+            a = self.aMaxVec[i]
+            v = self.vMaxVec[i]
+            s = self.distances[i]
+            dir = self.directions[i]
+            t_a = v / a  # individuelle Beschleunigungszeit
+            t_total = self.t_total_sync
+    
+            if self.triangleProfile:
+                if t < t_a:
+                    pos = 0.5 * a * t**2
+                elif t < t_total:
+                    t_dec = t - t_a
+                    pos = 0.5 * a * t_a**2 + a * t_a * t_dec - 0.5 * a * t_dec**2
+                else:
+                    pos = s
+            else:
+                s_a = 0.5 * a * t_a**2
+                t_const = t_total - 2 * t_a
+                if t < t_a:
+                    pos = 0.5 * a * t**2
+                elif t < t_a + t_const:
+                    pos = s_a + v * (t - t_a)
+                elif t < t_total:
+                    t_dec = t - (t_a + t_const)
+                    pos = s_a + v * t_const + v * t_dec - 0.5 * a * t_dec**2
+                else:
+                    pos = s
+    
+            lengths[i] = self.startLengths[i] + dir * pos
+
+        return lengths
